f420ca3ea7733a498e8d4edb0610b40f45882af3
Oluwafemi Sule <oluwafemisule@outlook.com>
2020-10-17 15:43:32 +0200

    Read identifier map cache file and watch for changes to it

---
+++ id_map.js
@@ -0,0 +1,41 @@
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+const ID_FILE = 'id_map.yml';
+const idMap = yaml.safeLoad(fs.readFileSync(ID_FILE, 'utf8'));
+
+const toIdMapFormat = (users) =>
+  users
+    .map((user) => ({
+      [user.screen_name]: user.id,
+    }))
+    .reduce((start, curr) => Object.assign(start, curr));
+
+const updateIdMap = users => {
+  fs.writeFileSync(
+    ID_FILE,
+    yaml.safeDump(Object.assign({}, idMap, toIdMapFormat(users))),
+    ENCODING
+  );
+};
+
+const absentInIdMap = screenName => idMap[screenName] === undefined
+
+const presentInIdMap = screenName => idMap[screenName] !== undefined
+
+const watchForChangesToIdMap = () => 
+  new Promise((resolve, reject) => {
+    fs.watch(ID_FILE, (eventType, filename) => {
+      resolve();
+    });
+  });
+
+const getId  = (screenName) => idMap[screenName]
+
+module.exports = {
+  getId,
+  updateIdMap,
+  presentInIdMap,
+  absentInIdMap,
+  watchForChangesToIdMap
+};
